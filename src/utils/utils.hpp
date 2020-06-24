31ff1fedc0f245ed1e0357630963df0d0301e864
Tina Han <tina.han.tiantian@gmail.com>
2020-06-24 17:56:30 -0400

    Fix include error in general utils file

--- src/utils/utils.hpp
+++ src/utils/utils.hpp
@@ -40,7 +40,7 @@ inline double random_double(double min, double max){
 
 //TODO: create includes folder?
 #include "..\ray.hpp"
-#include "utils\vec3.hpp"
-#include "utils\color.hpp"
+#include "vec3.hpp"
+#include "color.hpp"
 
 #endif
